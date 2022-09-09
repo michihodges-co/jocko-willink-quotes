bd9b2ee79500f8e9dd883be07b5dec6321d16f9b
michihodges <michihodges@gmail.com>
2022-09-09 16:31:34 +0200

    feat: Basic logic
    Basic logic has been added acting as the fundamental mechanics of the project.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('hello world ;)')
+
+const apples = 'Apples'
+const bananas = 'Bananas'
+const pears = 'Pears'
+
+let myArray = [apples, bananas, pears]
+
+let randomItem = myArray[Math.floor(Math.random()*myArray.length)]
+
+console.log(randomItem)
