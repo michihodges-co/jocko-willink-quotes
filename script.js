d6a0e69e23c7949691a67fe9669a0eedb4fc614a
michihodges <michihodges@gmail.com>
2022-09-13 23:33:43 +0200

    feat: Styling

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log('hello world ;)')
 
-const apples = 'Apples'
-const bananas = 'Bananas'
-const pears = 'Pears'
+const quote1 = 'Good'
+const quote2 = 'Discipline = Freedom'
+const quote3 = 'Get after it'
 
-let myArray = [apples, bananas, pears]
+let myArray = [quote1, quote2, quote3]
 
 let randomItem = myArray[Math.floor(Math.random()*myArray.length)]
 
 console.log(randomItem)
+document.write('<p>' + randomItem + '</p>')
