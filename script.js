33d6fd88851f9e31d1b953095ac8317fe587cea9
michihodges <michihodges@gmail.com>
2023-01-03 23:26:39 +0100

    feat: Content

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const quote1 = 'Good'
 const quote2 = 'Discipline equals freedom'
 const quote3 = 'Get after it'
 const quote4 = 'Believe'
-const quote5 = 'Cover and move'
-const quote6 = 'Simple'
-const quote7 = 'Prioritize and execute'
-const quote8 = 'Plan'
-const quote9 = 'Check the ego'
+const quote5 = 'Check the ego'
+const quote6 = 'Cover and move'
+const quote7 = 'Simple'
+const quote8 = 'Prioritize and execute'
+const quote9 = 'Plan'
 
 let myArray = [quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8, quote9]
 
