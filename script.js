b818a5e2b40acf42479f1f18dd1e9666c1d282ed
michihodges <michihodges@gmail.com>
2023-07-03 21:49:46 +0200

    feat: Content

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const quote4 = 'Do'
 const quote5 = 'Get after it'
 const quote6 = 'You'
 const quote7 = 'Go'
+const quote8 = 'Focus'
 
-let myArray = [quote1, quote2, quote3, quote4, quote5, quote6, quote7]
+let myArray = [quote1, quote2, quote3, quote4, quote5, quote6, quote7, quote8]
 
 let randomItem = myArray[Math.floor(Math.random()*myArray.length)]
 
